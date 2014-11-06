172cb2546531a2867d79c8f0ea3d0adfb26a8a6c
Libby H <libby@daydrea.me>
2014-11-05 20:34:50 -0500

    Added ability to filter list of places by activity.

--- app/views.py
+++ app/views.py
@@ -13,55 +13,55 @@ def query_db(query, args=(), one=False):
 def_query = ('select name, description from places ' + 
 	     'order by name')
 
-
 full_query = ('select places.name, places.description, ' +
-	      ' areas.name ' +
+	      'areas.name ' +
 	      'FROM places JOIN areas ' +
 	      'ON places.areaId = areas.id '
 	      'order by places.name')
 
-activity_list = ('select name from activities order by name')
-
-query_join = (' select joinActPlace.placeId '+
-	      ' FROM activities JOIN joinActPlace ' +
-	      ' ON activities.id = joinActPlace.activityId' +  
-	      ' where activities.name = "?"')
+query_join = (' select places.name, places.description, areas.name '+
+	      ' FROM places, areas JOIN joinActPlace, activities ' +
+	      ' ON places.id = joinActPlace.placeId AND ' +
+	      ' activities.id = joinActPlace.activityId ' +  
+	      ' where activities.name = ? AND areas.id = places.areaId')
 
 query_activities = ('select places.name, places.description, areas.name ' +
-		   'FROM places JOIN (?) as "a" ' +
+		   'FROM places JOIN ? as "a" ' +
 		   'ON places.id = a.placeId ' +
 		   'order by places.name')
+
+def get_activities_list():
+	cur = query_db('select name from activities order by name')
+	return [dict(name=row[0]) for row in cur]
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user = { 'nickname': 'Jesse' }
 	imageList = os.listdir('/home/libby/dev/outdoorRec/app/static/images/lrgImages')
 	cur = query_db(full_query)
 	places = [dict(name=row[0], 
 		       description=row[1],
 		       area=row[2]) for row in cur]
-	cur = query_db(activity_list)
-	activities = [dict(name=row[0]) for row in cur]
 	return render_template('index.html', 
 				title='Home',
-				activities=activities,
-				user=user,
+				activities=get_activities_list(),
 				places=places)
 
 @app.route('/places')
 @app.route('/places/<place>')
 def places(place=None):
-	return render_template('index.html')
+	return render_template('index.html',
+			       title=place,
+			       activities=get_activities_list())
 
 @app.route('/activities')
 @app.route('/activities/<activity>')
 def activity(activity=None):
 	cur = query_db(query_join, (activity,))
-	cur = query_db(query_activities, (cur,))
 	places = [dict(name=row[0], 
 		       description=row[1],
 		       area=row[2]) for row in cur]
 	return render_template('index.html',
 			       title=activity,
+			       activities=get_activities_list(),
 			       places=places )	
