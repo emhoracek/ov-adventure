f12a7ffb2537b4879e1c35cfdcdabd1f86d48cfc
Libby H <libby@daydrea.me>
2014-11-07 18:36:16 -0500

    Started adding pagination to place directory.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, g
 from app import app
-import os
+import os, math
 
+PLACES_PER_PAGE = 10
 
 def query_db(query, args=(), one=False):
     cur = g.db.execute(query, args)
@@ -19,7 +20,8 @@ full_query = ('select places.name, places.description, ' +
 	      'ON places.areaId = areas.id '
 	      'order by places.name')
 
-query_join = (' select places.name, places.description, areas.name '+
+query_join = (' select places.name, places.description, areas.name, '+
+	      ' places.latitude, places.longitude ' +
 	      ' FROM places, areas JOIN joinActPlace, activities ' +
 	      ' ON places.id = joinActPlace.placeId AND ' +
 	      ' activities.id = joinActPlace.activityId ' +  
@@ -47,40 +49,58 @@ def get_average_latlong(listplaces):
 	return { 'latitude': average_latitude,
 		 'longitude': average_longitude }
 
-def get_place_list(page):
+def get_place_list():
 	cur = query_db(full_query)
 	places = [dict(name=row[0], 
 		       description=row[1],
 		       area=row[2],
 		       latitude=row[3],
 		       longitude=row[4]) for row in cur]
-	placesPerPage = 10
-	maxPlaces = page * placesPerPage
-	startPlace = maxPlaces - placesPerPage
-	if ( len(places) > maxPlaces):
-		places = places[startPlace : maxPlaces]
+	return places
+
+def shorten_place_list(places, page):
+	maxPlaces = page * PLACES_PER_PAGE
+	startPlace = maxPlaces - PLACES_PER_PAGE
+	places = places[startPlace : maxPlaces]
 	return places
 
 @app.route('/')
 @app.route('/index')
 def index():
 	imageList = os.listdir('/home/libby/dev/outdoorRec/app/static/images/lrgImages')
-	places = get_place_list(1);
+	placesList = get_place_list()
+	numPlaces = (len (placesList) / PLACES_PER_PAGE)
+	numPages = math.ceil (float(len(placesList)) / PLACES_PER_PAGE)
+	hasPages = False
+	if (numPages > 1):
+		hasPages = True
+	places = shorten_place_list(placesList, 1)
 	return render_template('index.html', 
 				title='Home',
 				activities=get_activities_list(),
 				places=places,
+				numPlaces = numPlaces,
+				hasPages=hasPages,
+				numPages= (math.floor (numPages)),
 				map_center = get_average_latlong(places) )
 
 @app.route('/page')
 @app.route('/page/<page>')
 def page(page=1):
-	places = get_place_list(page);
+	page = int(page)
+	placesList = get_place_list()
+	places = shorten_place_list(placesList, page)
+	numPages = math.ceil (float(len(placesList))/ PLACES_PER_PAGE)
+	hasPages = False
+	if (numPages > 1):
+		hasPages = True
 	return render_template('index.html',
 			       activities=get_activities_list(),
 			       places=places,
-			       ),
-			        
+			       page=page,
+			       numPages = numPages,
+			       map_center = get_average_latlong(places)
+			       )			        
 
 @app.route('/places')
 @app.route('/places/<place>')
@@ -93,11 +113,21 @@ def places(place=None):
 @app.route('/activities/<activity>')
 def activity(activity=None):
 	cur = query_db(query_join, (activity,))
-	places = [dict(name=row[0], 
+	placesList = [dict(name=row[0], 
 		       description=row[1],
-		       area=row[2]) for row in cur]
+		       area=row[2],
+		       latitude=row[3],
+		       longitude=row[4]) for row in cur]
+	places = shorten_place_list(placesList, 1)
+	numPages = math.ceil (float(len(placesList))/ PLACES_PER_PAGE)
+	hasPages = False
+	if (numPages > 1):
+		hasPages = True
 	return render_template('index.html',
 			       title=activity,
 			       activities=get_activities_list(),
 			       activity=activity,
-			       places=places )	
+			       places=places,
+			       page=page,
+			       numPages = numPages,
+			       map_center = get_average_latlong(places))
