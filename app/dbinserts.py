c584d4287b86353e009bd6597725f2f6a579c1f6
Libby H <libby@daydrea.me>
2015-03-21 16:02:40 -0400

    Removed extra folder.

--- app/dbinserts.py
+++
@@ -1,7 +0,0 @@
-from flask import g
-
-add_new_place = ('insert into places (id, name, description, ' +
-                 'countyId, latitude, longitude) ' +
-                 'values (?, ?, ?, ? ,?,?)')
-
-add_new_user = 'insert into users (username, password) values (?, ?)'
